9a9fbc06ca90a460b2a96bbb2f889c07c70e7ff8
louis <louis@warp.hk>
2017-10-13 23:04:56 +0200

    first agent class

--- dqnGym.py
+++ dqnGym.py
@@ -5,3 +5,12 @@ import random
 import keras
 
 
+
+class Agent:
+    def __init__(self, state_size, action_size):
+
+        self.gamma = 0.99
+        self.explorationRate = 1.0 #to replace with expo
+        self.decay = 0.99
+        self.learning_rate = 0.03
+
